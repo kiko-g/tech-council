011b412ddc7e2e5fe26b9a0209eeaa6b7af3ceee
kiko-g <kikojpgoncalves@gmail.com>
2021-02-28 22:26:22 +0000

    added first sketch; footer html/css done

--- html/index.php
+++ html/index.php
@@ -1,13 +1,41 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>TechCouncil</title>
-  </head>
-  <body>
-    <p>Content from HTML.</p>
-  <?php
-    echo '<p>content from PHP.</p>';
-  ?>
-  </body>
+<?php require 'templates/head.html'; ?>
+<?php require 'templates/header.html'; ?>
+
+<body>
+  <div id="dummy">
+    Pages
+    <ul id="list">
+      <li><a href="home.html">Home</a> Page</li>
+      <li><a href="profile.html">Profile</a> Page</li>
+      <li><a href="question.html">Question</a> Page</li>
+    </ul>
+  </div>
+
+  <main>
+    <article class="mural">
+      <section class="question">
+        <h3>Is Java a good programming language?</h3>
+        <p>I was wondering if I should study it</p>
+      </section>
+      <section class="question"></section>
+    </article>
+
+    <aside class="sidebar">
+      <section class="themes">
+        <ul>
+          <li>OS</li>
+          <li>Mobile</li>
+          <li>Consoles</li>
+        </ul>
+      </section>
+    </aside>
+  </main>
+
+  <div id="test">
+    <button type="button" class="btn btn-primary">Primary</button>
+  </div>
+</body>
+
+<?php require 'templates/footer.html'; ?>
+
 </html>
