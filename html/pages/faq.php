5d61f2893185d6ba269a1468e955d54cee04468e
TsarkFC <up201806779@fe.up.pt>
2021-03-05 19:30:47 +0000

    about and faq pages

---
+++ html/pages/faq.php
@@ -0,0 +1,50 @@
+<?php require_once '../templates/head.php'; ?>
+
+<?php
+$questions = array(
+	"How do I ask a question?" => "	There is a 'Pose a Question' button on the top of the home page, once clicked you will be redirected to the question creation page.",
+	"Can my account be unbanned?" => "Yes. If one of ours moderators finds that you are in condition to interact once again with our platform, he has the permission to unban you.",
+	"Can I delete a question/answer?" => "Yes. Once you delete a question its content is deleted but answers will have suffer no changes. If you delete an answer its content is deleted but wisdom points information is kept."
+);
+$questionCounter = 0;
+?>
+
+<body>
+	<div class="d-flex entry-form flex-column justify-content-center">
+		<header class="text-light mb-5">
+			<h1>Tech Council</h1>
+		</header>
+
+		<div class="accordion accordion-flush" id="accordionFAQ">
+			<header class="text-start text-light mb-4 ms-4">
+				<h3>FAQ</h3>
+			</header>
+
+			<?php
+			foreach ($questions as $question => $answer) {
+				$questionCounter++;
+			?>
+
+				<div class="accordion-item btn-light mb-2 border border-5 rounded-3">
+					<h2 class="accordion-header" id="heading<?= $questionCounter ?>">
+						<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?= $questionCounter ?>" aria-expanded="false" aria-controls="collapse<?= $questionCounter ?>">
+							<?= $question ?>
+						</button>
+					</h2>
+					<div id="collapse<?= $questionCounter ?>" class="accordion-collapse collapse" aria-labelledby="heading<?= $questionCounter ?>" data-bs-parent="#accordionFAQ">
+						<div class="accordion-body text-start">
+							<?= $answer ?>
+						</div>
+					</div>
+				</div>
+
+			<?php
+			}
+			?>
+		</div>
+	</div>
+
+	<?php require_once '../templates/footer.php'; ?>
+</body>
+
+</html>
--- html/pages/login.php
+++ html/pages/login.php
@@ -1,29 +1,27 @@
 <?php require_once '../templates/head.php'; ?>
 
 <body>
-	<div class="entry-form">
-		<div>
-			<header class="entry-header">
-				<h1>Tech Council</h1>
+	<div class="d-flex entry-form flex-column justify-content-center">
+		<header class="text-light mb-5">
+			<h1>Tech Council</h1>
+		</header>
+		<form>
+			<header class="text-start text-light mb-4 ms-4">
+				<h3>Sign in</h3>
 			</header>
-			<form>
-				<header class="form-header">
-					<h3>Sign in</h3>
-				</header>
-				<div class="form-floating mb-4">
-					<input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-					<label for="floatingInput">Email address</label>
-				</div>
-				<div class="form-floating mb-4">
-					<input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-					<label for="floatingPassword">Password</label>
-				</div>
-				<div class="form-redirect mb-4">
-					<a href="./register.php" class="entry-redirect">Don't have an account? <br> Sign up</a>
-					<button type="submit" class="btn btn-outline-primary btn-light">Submit</button>
-				</div>
-			</form>
-		</div>
+			<div class="form-floating mb-4">
+				<input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+				<label for="floatingInput">Email address</label>
+			</div>
+			<div class="form-floating mb-4">
+				<input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+				<label for="floatingPassword">Password</label>
+			</div>
+			<div class="d-flex justify-content-evenly">
+				<a href="./register.php" class="link-light entry-anchor">Don't have an account? <br> Sign up</a>
+				<button type="submit" class="btn btn-outline-primary btn-light">Submit</button>
+			</div>
+		</form>
 	</div>
 	<?php require_once '../templates/footer.php'; ?>
 </body>
