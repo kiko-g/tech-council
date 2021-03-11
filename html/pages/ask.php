29f1936ccd3a4595e75a7c6a9f682ac8d6a8b25e
TsarkFC <up201806779@fe.up.pt>
2021-03-11 16:48:36 +0000

    ask a question page initial structure

--- html/pages/ask.php
+++ html/pages/ask.php
@@ -1,88 +1,70 @@
-<?php require_once '../templates/head.php'; ?>
+<?php require '../templates/head.php' ?>
 
-<?php function buildQuestion($comments)
-{ ?>
-  <div class="card mb-4 p-2-0 border-0 rounded">
-    <div class="card-header bg-petrol text-white font-open-sans">
-      How do I compare strings in Java?
-    </div>
-    <div class="card-body">
-      <p class="mb-3">
-        I've been using the <code>==</code> operator in my program to compare all my strings so far.
-        However, I ran into a bug, changed one of them into <code>.equals()</code> instead, and it fixed the bug.
-
-        Is <code>==</code> bad? When should it and should it not be used? What's the difference?
-      </p>
-      <div class="row row-cols-3 mb-3">
-        <div class="col-sm flex-wrap">
-          <div id="votes" class="votes btn-group-special btn-group-vertical-when-responsive mt-1 flex-wrap">
-            <a id="upvote-button" class="upvote-button my-btn-pad btn btn-outline-success teal" onclick="vote('up', this.parentNode)">
-              <i class="fas fa-chevron-up"></i>
-            </a>
-            <a id="vote-ratio" class="vote-ratio btn btn-secondary my-btn-pad fake disabled"> 45 </a>
-            <a id="downvote-button" class="downvote-button my-btn-pad btn btn-outline-danger pink" onclick="vote('down', this.parentNode)">
-              <i class="fas fa-chevron-down"></i>
-            </a>
+<body>
+  <?php require '../templates/header.php' ?>
+  <main class="container">
+    <div class="row">
+      <article class="col-lg-9">
+        <div class="card mb-4 p-2-0 border-0 rounded">
+          <div class="card-header bg-petrol text-white font-open-sans">
+            Ask a question
           </div>
-        </div>
-
-        <div id="tags" class="col-sm-auto">
-          <div class="btn-group mt-1">
-            <a href="#" class="btn btn-secondary border-0 my-btn-pad2">java</a>
-          </div>
-          <div class="btn-group mt-1">
-            <a href="#" class="btn btn-secondary border-0 my-btn-pad2">node</a>
-          </div>
-          <div class="btn-group mt-1">
-            <a href="#" class="btn btn-secondary border-0 my-btn-pad2">msi</a>
-          </div>
-          <div class="btn-group mt-1">
-            <a href="#" class="btn btn-secondary border-0 my-btn-pad2">nvidia</a>
-          </div>
-        </div>
-      </div>
-      <?php
-      if ($comments != null) buildCommentSection($comments, 1);
-      ?>
-      <div class="row row-cols-3 justify-content-start">
-        <div id="interact" class="col-sm-auto">
-          <div class="btn-group mt-1 rounded">
-            <a class="upvote-button my-btn-pad2 btn btn-outline-success star" id="upvote-button-<ID>" onclick="toggleStar(this)" href="#">
-              <i class="far fa-star"></i>&nbsp;Save
-            </a>
-          </div>
-          <div class="btn-group mt-1 rounded">
-            <a class="upvote-button btn wine my-btn-pad2" id="upvote-button-<ID>" onclick="/**/" href="#">
-              <i class="far fa-comment-dots"></i>&nbsp;25
-            </a>
-          </div>
-          <?php if ($comments == null) { ?>
-            <div class="btn-group mt-1 rounded">
-              <a class="upvote-button btn teal my-btn-pad2" id="upvote-button-<ID>" onclick="/**/" href="#">
-                <i class="fas fa-reply"></i>&nbsp;Reply
-              </a>
+          <div class="card-body">
+            <form>
+              <textarea class="form-control shadow-sm border border-2 bg-light mb-2" rows="1" placeholder="Question title"></textarea>
+              <textarea class="form-control shadow-sm border border-2 bg-light mb-2" rows="5" placeholder="Question body"></textarea>
+              <div class="row row-cols-auto mb-3">
+                <div id="tags" class="col-sm-auto">
+                  <div class="btn-group mt-1" data-bs-toggle="collapse" data-bs-target="#addTag" aria-expanded="false" id="tag-select">
+                    <a class="btn blue border-0 my-btn-pad2"><i class="fas fa-plus-square"></i>&nbsp;add tag</a>
+                  </div>
+                  <div class="btn-group mt-1" data-bs-toggle="collapse" data-bs-target="#addTag" aria-expanded="false" id="tag-close">
+                    <a class="btn btn-danger border-0 my-btn-pad2"><i class="fas fa-window-close"></i>&nbsp;close</a>
+                  </div>
+                  <div class="tag-selected btn-group mt-1">
+                    <a class="btn btn-secondary border-0 my-btn-pad2"><i class="fas fa-minus-square"></i>&nbsp;java</a>
+                  </div>
+                  <div class="tag-selected btn-group mt-1">
+                    <a class="btn btn-secondary border-0 my-btn-pad2"><i class="fas fa-minus-square"></i>&nbsp;node</a>
+                  </div>
+                  <div class="tag-selected btn-group mt-1">
+                    <a" class="btn btn-secondary border-0 my-btn-pad2"><i class="fas fa-minus-square"></i>&nbsp;msi</a>
+                  </div>
+                  <div class="tag-selected btn-group mt-1">
+                    <a class="btn btn-secondary border-0 my-btn-pad2"><i class="fas fa-minus-square"></i>&nbsp;nvidia</a>
+                  </div>
+                </div>
+                <a class="btn btn-success teal text-white ms-auto me-3 mt-1" role="button">
+                  Submit
+                </a>
+              </div>
+            </form>
+            <div class="card collapse" id="addTag" style="width: 18rem;">
+              <div class="card-body">
+                <input type="text" class="form-control shadow-sm border border-2 bg-light mb-2" rows="1" placeholder="Search tag"></input>
+                <div id="tags" class="col-sm-auto">
+                  <div class="search-tag btn-group mt-1">
+                    <a class="btn btn-secondary border-0 my-btn-pad2"><i class="fas fa-plus-square"></i>&nbsp;unix</a>
+                  </div>
+                  <div class="search-tag btn-group mt-1">
+                    <a class="btn btn-secondary border-0 my-btn-pad2"><i class="fas fa-plus-square"></i>&nbsp;ps4</a>
+                  </div>
+                  <div class="search-tag btn-group mt-1">
+                    <a class="btn btn-secondary border-0 my-btn-pad2"><i class="fas fa-plus-square"></i>&nbsp;dell</a>
+                  </div>
+                  <div class="search-tag btn-group mt-1">
+                    <a class="btn btn-secondary border-0 my-btn-pad2"><i class="fas fa-plus-square"></i>&nbsp;apple</a>
+                  </div>
+                </div>
+              </div>
             </div>
-          <?php } ?>
-          <div class="btn-group mt-1 rounded">
-            <a class="upvote-button btn blue my-btn-pad2" id="upvote-button-<ID>" onclick="/**/" href="#">
-              <i class="fas fa-share-alt"></i>&nbsp;Share
-            </a>
           </div>
         </div>
-      </div>
+      </article>
+      <?php require '../templates/aside.php' ?>
     </div>
-
-    <div class="card-footer text-muted text-end p-0">
-      <blockquote class="blockquote mb-0">
-        <p class="card-text px-1">
-          <small class="text-muted">34 seconds ago</small>
-          <small>
-            <a class="signature" href="#">user</a>
-          </small>
-        </p>
-      </blockquote>
-    </div>
-  </div>
-<?php } ?>
+  </main>
+  <?php require '../templates/footer.php' ?>
+</body>
 
 </html>
--- html/templates/head.php
+++ html/templates/head.php
@@ -34,6 +34,7 @@
   <link rel="stylesheet" href="../style/aside.css" />
   <link rel="stylesheet" href="../style/search.css" />
   <link rel="stylesheet" href="../style/question-page.css" />
+  <link rel="stylesheet" href="../style/ask.css" />
 
   <!-- JS includes -->
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
@@ -42,5 +43,6 @@
   <!-- Scripts -->
   <script src="../js/script.js" defer></script>
   <script src="../js/moderator-switch.js" defer></script>
+  <script src="../js/tag-selector.js" defer></script>
 
 </head>
