337d64ff0bb4d7c077db7f3aab00df77cd5edf57
TsarkFC <up201806779@fe.up.pt>
2021-04-13 17:43:29 +0100

    finished reputation related triggers

--- sql/test.sql
+++ sql/test.sql
@@ -1,11 +1,30 @@
-INSERT INTO photo (path) VALUES ('photo.png');
-INSERT INTO "user" (email, "name", "password", reputation, profile_photo) VALUES ('test', 'misterarnildo', '123', 0, 1);
-INSERT INTO "user" (email, "name", "password", reputation, profile_photo) VALUES ('mail', 'user', '123', 3, 1);
+--TEST UPVOTE RELATED TRIGGERS
+INSERT INTO photo (id, path) VALUES (1, 'photo.png');
+INSERT INTO "user" (email, "name", "password", reputation, bio, expert, banned, profile_photo) VALUES ('test', 'misterarnildo', '123', 1499, 'wanna be expert', FALSE, FALSE, 1);
+INSERT INTO "user" (email, "name", "password", reputation, bio, expert, banned, profile_photo) VALUES ('mail', 'user', '123', 1400, 'im goin down', TRUE, FALSE, 1);
 INSERT INTO content (id, main, author_id) VALUES (50, 'question', 1);
-INSERT INTO content (id, main, author_id) VALUES (51, 'answer', 1);
+INSERT INTO content (id, main, author_id) VALUES (51, 'answer', 2);
 INSERT INTO question (content_id, title) VALUES (50, 'title');
 INSERT INTO answer (content_id, question_id) VALUES (51, 50);
 
+--test insert operations
 INSERT INTO user_vote_question ("user_id", question_id, upvote) VALUES (1, 50, 1);
 INSERT INTO user_vote_question ("user_id", question_id, upvote) VALUES (2, 50, 1);
 INSERT INTO user_vote_answer ("user_id", answer_id, upvote) VALUES (1, 51, -1);
+SELECT * FROM "user"; --user 1 rep has to be 1501 and is now expert, user 2 rep has to be 1399 and is not expert
+SELECT * FROM question; --question 50 rep has to be 2
+SELECT * FROM answer; --answer 51 rep has to be -1
+
+--test update operations
+UPDATE user_vote_question SET upvote = -1 WHERE "user_id" = 1;
+UPDATE user_vote_answer SET upvote = 1 WHERE "user_id" = 1;
+SELECT * FROM "user"; --user 1 rep has to be 1499, user 2 rep has to be 1401
+SELECT * FROM question; --question 50 rep has to be 0
+SELECT * FROM answer; --answer 51 rep has to be 1
+
+--test delete operations
+DELETE FROM user_vote_question WHERE "user_id" = 1;
+DELETE FROM user_vote_answer WHERE "user_id" = 1;
+SELECT * FROM "user"; --user 1 rep has to be 1500, user 2 rep has to be 1400
+SELECT * FROM question; --question 50 rep has to be 1
+SELECT * FROM answer; --answer 51 rep has to be 0
--- sql/triggers.sql
+++ sql/triggers.sql
@@ -1,109 +1,239 @@
 ---------------------------
--- UPDATE USER (AND QUESTION?) REPUTATION 
+-- UPDATE USER AND QUESTION REPUTATION 
 ---------------------------
 
 -- on insert to question table
-
-DROP TRIGGER IF EXISTS insert_reputation_question ON user_vote_question;
 DROP FUNCTION IF EXISTS insert_reputation_question CASCADE;
-
 CREATE FUNCTION insert_reputation_question() RETURNS TRIGGER AS
 $BODY$
 BEGIN
-    IF NEW.upvote <> 0 THEN
-        UPDATE "user" SET reputation = reputation + NEW.upvote
-        WHERE "user".id = (SELECT author_id FROM content 
-                            WHERE (content.id = NEW.question_id)); 
-    END IF;
+    -- update user reputation
+    UPDATE "user" SET reputation = reputation + NEW.upvote
+    WHERE "user".id = (SELECT author_id FROM content 
+                        WHERE (content.id = NEW.question_id));
+
+    --verify if user is now expert
+    UPDATE "user" SET expert = TRUE
+    WHERE "user".id = (SELECT author_id FROM content 
+                        WHERE (content.id = NEW.question_id))
+        AND "user".expert = FALSE
+        AND "user".reputation >= 1500;
+
+    --verify if user has lost expert status
+    UPDATE "user" SET expert =  FALSE
+    WHERE "user".id = (SELECT author_id FROM content 
+                        WHERE (content.id = NEW.question_id))
+        AND "user".expert = TRUE
+        AND "user".reputation < 1400;
+
+    --update question votes difference
+    UPDATE question SET votes_difference = votes_difference + NEW.upvote
+    WHERE content_id = NEW.question_id;
 
     RETURN NEW;
 END
 $BODY$
 LANGUAGE plpgsql;
- 
+
+DROP TRIGGER IF EXISTS insert_reputation_question ON user_vote_question;
 CREATE TRIGGER insert_reputation_question
     AFTER INSERT ON user_vote_question
     FOR EACH ROW
     EXECUTE PROCEDURE insert_reputation_question();
 
 -- on insert to answer table
-
-DROP TRIGGER IF EXISTS insert_reputation_answer ON user_vote_answer;
 DROP FUNCTION IF EXISTS insert_reputation_answer CASCADE;
-
 CREATE FUNCTION insert_reputation_answer() RETURNS TRIGGER AS
 $BODY$
 BEGIN
-    IF NEW.upvote <> 0 THEN
-        UPDATE "user" SET reputation = reputation + NEW.upvote
-        WHERE "user".id = (SELECT author_id FROM content 
-                            WHERE (content.id = NEW.answer_id)); 
-    END IF;
+    -- update user reputation
+    UPDATE "user" SET reputation = reputation + NEW.upvote
+    WHERE "user".id = (SELECT author_id FROM content 
+                        WHERE (content.id = NEW.answer_id));
+
+    --verify if user is now expert
+    UPDATE "user" SET expert = TRUE
+    WHERE "user".id = (SELECT author_id FROM content 
+                        WHERE (content.id = NEW.answer_id))
+        AND "user".expert = FALSE
+        AND "user".reputation >= 1500;
+
+    --verify if user has lost expert status
+    UPDATE "user" SET expert =  FALSE
+    WHERE "user".id = (SELECT author_id FROM content 
+                        WHERE (content.id = NEW.answer_id))
+        AND "user".expert = TRUE
+        AND "user".reputation < 1400;
+
+    --update answer votes difference
+    UPDATE answer SET votes_difference = votes_difference + NEW.upvote
+    WHERE content_id = NEW.answer_id;
 
     RETURN NEW;
 END
 $BODY$
 LANGUAGE plpgsql;
- 
+
+DROP TRIGGER IF EXISTS insert_reputation_answer ON user_vote_answer;
 CREATE TRIGGER insert_reputation_answer
     AFTER INSERT ON user_vote_answer
     FOR EACH ROW
     EXECUTE PROCEDURE insert_reputation_answer();
 
 -- on update to question table
-
-DROP TRIGGER IF EXISTS update_reputation_question ON user_vote_question;
 DROP FUNCTION IF EXISTS update_reputation_question CASCADE;
-
 CREATE FUNCTION update_reputation_question() RETURNS TRIGGER AS
 $BODY$
 DECLARE rep integer := NEW.upvote - OLD.upvote;
 BEGIN
-    IF rep <> 0 THEN
-        UPDATE "user" SET reputation = reputation + rep
-        WHERE "user".id = (SELECT author_id FROM content 
-                            WHERE (content.id = NEW.question_id)); 
-    END IF;
+    -- update user reputation
+    UPDATE "user" SET reputation = reputation + rep
+    WHERE "user".id = (SELECT author_id FROM content 
+                        WHERE (content.id = NEW.question_id));
+
+    --verify if user is now expert
+    UPDATE "user" SET expert = TRUE
+    WHERE "user".id = (SELECT author_id FROM content 
+                        WHERE (content.id = NEW.question_id))
+        AND "user".expert = FALSE
+        AND "user".reputation >= 1500;
+
+    --verify if user has lost expert status
+    UPDATE "user" SET expert =  FALSE
+    WHERE "user".id = (SELECT author_id FROM content 
+                        WHERE (content.id = NEW.question_id))
+        AND "user".expert = TRUE
+        AND "user".reputation < 1400;
+
+    --update question votes difference
+    UPDATE question SET votes_difference = votes_difference + rep
+    WHERE content_id = NEW.question_id;
 
     RETURN NEW;
 END
 $BODY$
 LANGUAGE plpgsql;
 
+DROP TRIGGER IF EXISTS update_reputation_question ON user_vote_question;
 CREATE TRIGGER update_reputation_question
     AFTER UPDATE ON user_vote_question
     FOR EACH ROW
     EXECUTE PROCEDURE update_reputation_question();
 
 -- on update to answer table
-
-DROP TRIGGER IF EXISTS update_reputation_answer ON user_vote_answer;
 DROP FUNCTION IF EXISTS update_reputation_answer CASCADE;
-
 CREATE FUNCTION update_reputation_answer() RETURNS TRIGGER AS
 $BODY$
 DECLARE rep integer := NEW.upvote - OLD.upvote;
 BEGIN
-    IF rep <> 0 THEN
-        UPDATE "user" SET reputation = reputation + rep
-        WHERE "user".id = (SELECT author_id FROM content 
-                            WHERE (content.id = NEW.answer_id)); 
-    END IF;
+    -- update user reputation
+    UPDATE "user" SET reputation = reputation + rep
+    WHERE "user".id = (SELECT author_id FROM content 
+                        WHERE (content.id = NEW.answer_id));
+
+    --verify if user is now expert
+    UPDATE "user" SET expert = TRUE
+    WHERE "user".id = (SELECT author_id FROM content 
+                        WHERE (content.id = NEW.answer_id))
+        AND "user".expert = FALSE
+        AND "user".reputation >= 1500;
+
+    --verify if user has lost expert status
+    UPDATE "user" SET expert =  FALSE
+    WHERE "user".id = (SELECT author_id FROM content 
+                        WHERE (content.id = NEW.answer_id))
+        AND "user".expert = TRUE
+        AND "user".reputation < 1400;
+
+    --update answer votes difference
+    UPDATE answer SET votes_difference = votes_difference + rep
+    WHERE content_id = NEW.answer_id;
 
     RETURN NEW;
 END
 $BODY$
 LANGUAGE plpgsql;
 
+DROP TRIGGER IF EXISTS update_reputation_answer ON user_vote_answer;
 CREATE TRIGGER update_reputation_answer
     AFTER UPDATE ON user_vote_answer
     FOR EACH ROW
     EXECUTE PROCEDURE update_reputation_answer();
 
 -- on delete from question table
+DROP FUNCTION IF EXISTS delete_reputation_question CASCADE;
+CREATE FUNCTION delete_reputation_question() RETURNS TRIGGER AS
+$BODY$
+BEGIN
+    -- update user reputation
+    UPDATE "user" SET reputation = reputation - OLD.upvote
+    WHERE "user".id = (SELECT author_id FROM content 
+                        WHERE (content.id = OLD.question_id));
+
+    --verify if user is now expert
+    UPDATE "user" SET expert = TRUE
+    WHERE "user".id = (SELECT author_id FROM content 
+                        WHERE (content.id = OLD.question_id))
+        AND "user".expert = FALSE
+        AND "user".reputation >= 1500;
+
+    --verify if user has lost expert status
+    UPDATE "user" SET expert =  FALSE
+    WHERE "user".id = (SELECT author_id FROM content 
+                        WHERE (content.id = OLD.question_id))
+        AND "user".expert = TRUE
+        AND "user".reputation < 1400;
+
+    --update question votes difference
+    UPDATE question SET votes_difference = votes_difference - OLD.upvote
+    WHERE content_id = OLD.question_id;
+
+    RETURN NEW;
+END
+$BODY$
+LANGUAGE plpgsql;
+ 
+DROP TRIGGER IF EXISTS delete_reputation_question ON user_vote_question;
+CREATE TRIGGER delete_reputation_question
+    AFTER DELETE ON user_vote_question
+    FOR EACH ROW
+    EXECUTE PROCEDURE delete_reputation_question();
 
 -- on delete from answer table
+DROP FUNCTION IF EXISTS delete_reputation_answer CASCADE;
+CREATE FUNCTION delete_reputation_answer() RETURNS TRIGGER AS
+$BODY$
+BEGIN
+    -- update user reputation
+    UPDATE "user" SET reputation = reputation - OLD.upvote
+    WHERE "user".id = (SELECT author_id FROM content 
+                        WHERE (content.id = OLD.answer_id));
 
----------------------------
--- Set user default photo
----------------------------
+    --verify if user is now expert
+    UPDATE "user" SET expert = TRUE
+    WHERE "user".id = (SELECT author_id FROM content 
+                        WHERE (content.id = OLD.answer_id))
+        AND "user".expert = FALSE
+        AND "user".reputation >= 1500;
+
+    --verify if user has lost expert status
+    UPDATE "user" SET expert =  FALSE
+    WHERE "user".id = (SELECT author_id FROM content 
+                        WHERE (content.id = OLD.answer_id))
+        AND "user".expert = TRUE
+        AND "user".reputation < 1400;
+
+    --update answer votes difference
+    UPDATE answer SET votes_difference = votes_difference - OLD.upvote
+    WHERE content_id = OLD.answer_id;
+
+    RETURN OLD;
+END
+$BODY$
+LANGUAGE plpgsql;
+ 
+DROP TRIGGER IF EXISTS delete_reputation_answer ON user_vote_answer;
+CREATE TRIGGER delete_reputation_answer
+    AFTER DELETE ON user_vote_answer
+    FOR EACH ROW
+    EXECUTE PROCEDURE delete_reputation_answer();
