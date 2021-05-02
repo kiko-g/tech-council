707eae45df9ea734f34e6673147df298cdf64c14
kiko-g <kikojpgoncalves@gmail.com>
2021-05-03 00:01:47 +0100

    minor fixes

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header>
   <nav class="bg-animated navbar navbar-expand-lg navbar-dark bg-dark p-2">
     <div class="container-fluid justify-content-center px-3">
-      <a class="navbar-brand" href="{{route('home')}}">
+      <a class="navbar-brand" href="{{ route('home') }}">
         @include('partials.icon')
         <!-- <img src="/images/icon.png" alt="" width="23" height="23" class="d-inline-block align-top mt-1"> -->
         Tech Council
@@ -22,15 +22,15 @@
 
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0 text-black">
           @auth
-            @if($user->moderator)
-              <li class="nav-item">     
-                  <a class="nav-link hover-cute btn btn-outline-light btn-sm border-0" href="{{route('home')}}"><i
-                      class="fas fa-briefcase fa-sm"></i>&nbsp;Moderator</a>
+            @if ($user->moderator)
+              <li class="nav-item">
+                <a class="nav-link hover-cute btn btn-outline-light btn-sm border-0" href="{{ route('home') }}"><i
+                    class="fas fa-briefcase fa-sm"></i>&nbsp;Moderator</a>
               </li>
             @endif
             <li class="nav-item">
-            <a href="{{route('logout')}}" class="nav-link hover-cute btn btn-outline-light btn-sm border-0"
-              type="submit"><i class="fas fa-sign-out-alt fa-sm"></i>&nbsp;Logout</a>
+              <a href="{{ route('logout') }}" class="nav-link hover-cute btn btn-outline-light btn-sm border-0"
+                type="submit"><i class="fas fa-sign-out-alt fa-sm"></i>&nbsp;Logout</a>
             </li>
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle btn btn-outline-light nohover btn-sm border-0" data-bs-toggle="dropdown"
@@ -49,19 +49,18 @@
               </ul>
             </li>
             <li class="nav-item">
-              <a href="/pages/profile.php" class="nav-link hover-cute btn btn-outline-light btn-sm border-0"
-                type="submit">
+              <a href="#" class="nav-link hover-cute btn btn-outline-light btn-sm border-0" type="submit">
                 Profile
                 <img src="/images/dwight.png" alt="" width="23" height="23" class="">
               </a>
             </li>
           @else
             <li class="nav-item">
-              <a href="{{route('login')}}" class="nav-link hover-cute btn btn-outline-light btn-sm border-0"
+              <a href="{{ route('login') }}" class="nav-link hover-cute btn btn-outline-light btn-sm border-0"
                 type="submit"><i class="fas fa-sign-in-alt fa-sm"></i>&nbsp;Login</a>
             </li>
             <li class="nav-item">
-              <a href="{{route('register')}}" class="nav-link hover-cute btn btn-outline-light btn-sm border-0"
+              <a href="{{ route('register') }}" class="nav-link hover-cute btn btn-outline-light btn-sm border-0"
                 type="submit"><i class="fas fa-user-plus fa-sm"></i>&nbsp;Register</a>
             </li>
           @endauth
