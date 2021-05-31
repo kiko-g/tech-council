25e92a206bc0977db36dd7152b18b88de00235f2
kiko-g <kikojpgoncalves@gmail.com>
2021-05-31 02:57:59 +0100

    fix delete modal

--- resources/views/partials/question/delete-modal.blade.php
+++ resources/views/partials/question/delete-modal.blade.php
@@ -9,32 +9,18 @@ if ($type == 'question') {
 @endphp
 
 <div class="modal fade" id="delete-{{ $type }}-modal-{{ $content_id }}" tabindex="-1"
-    aria-labelledby="delete-{{ $type }}-modal-{{ $content_id }}-label" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title text-danger" id="delete-{{ $type }}-modal-{{ $content_id }}-label">Delete {{ $type }}</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body text-dark">
-                {{ $before_title }} {{ $title }}
-                <div class="alert alert-warning mt-2" role="alert">
-                    {{ $warning_message }}
-                </div>
-            </div>
-            <div class="modal-footer">
-                <form class="{{ $type }}-delete" id="{{ $type }}-delete-{{ $content_id }}" method="post"
-					action="{{ url('/' . $type . '/' . $content_id . '/delete') }}">
-                    @method('DELETE')
-                    @csrf
-                    <button class="btn btn-success @if (!$redirect) delete-modal @endif" 
-						id="delete-{{ $type }}-{{ $content_id }}"
-                        @if (!$redirect) data-bs-dismiss="modal" @endif type="submit">
-                        Delete
-                    </button>
-                </form>
-                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
-            </div>
+  aria-labelledby="delete-{{ $type }}-modal-{{ $content_id }}-label" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title text-danger" id="delete-{{ $type }}-modal-{{ $content_id }}-label">Delete
+          {{ $type }}</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body text-dark">
+        {{ $before_title }} {{ $title }}
+        <div class="alert alert-warning mt-2" role="alert">
+          {{ $warning_message }}
         </div>
       </div>
       <div class="modal-footer">
@@ -42,12 +28,23 @@ if ($type == 'question') {
           action="{{ url('/' . $type . '/' . $content_id . '/delete') }}">
           @method('DELETE')
           @csrf
-          <button class="btn btn-success teal @if (!$redirect) delete-modal @endif" id="delete-{{ $type }}-{{ $content_id }}" @if (!$redirect) data-bs-dismiss="modal" @endif type="submit">
+          <button class="btn btn-success @if (!$redirect) delete-modal @endif" id="delete-{{ $type }}-{{ $content_id }}" @if (!$redirect) data-bs-dismiss="modal" @endif type="submit">
             Delete
           </button>
         </form>
-        <button type="button" class="btn btn-danger wine" data-bs-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
       </div>
     </div>
+  </div>
+  <div class="modal-footer">
+    <form class="{{ $type }}-delete" id="{{ $type }}-delete-{{ $content_id }}" method="post"
+      action="{{ url('/' . $type . '/' . $content_id . '/delete') }}">
+      @method('DELETE')
+      @csrf
+      <button class="btn btn-success teal @if (!$redirect) delete-modal @endif" id="delete-{{ $type }}-{{ $content_id }}" @if (!$redirect) data-bs-dismiss="modal" @endif type="submit">
+        Delete
+      </button>
+    </form>
+    <button type="button" class="btn btn-danger wine" data-bs-dismiss="modal">Cancel</button>
   </div>
 </div>
